0a6b976347ed20f9590aa9d9cb4e62fa4f98d9ea
chunkitmax <chunkitmax1125@gmail.com>
2020-02-29 17:12:51 +0800

    update pb2ts.sh

--- tools/pb2ts.sh
+++ tools/pb2ts.sh
@@ -7,6 +7,10 @@ cd $DIR/..
 if [ ! -d "py-futu-api" ]
 then
   git clone --single-branch --branch v4.x https://github.com/FutunnOpen/py-futu-api.git
+else
+  cd py-futu-api
+  git pull origin v4.x
+  cd ..
 fi
 
 if [ -z $( command -v pbts ) ]
