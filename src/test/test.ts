a83d6fbdf4f4ea9381043df2b98cf673a3b13e04
chunkitmax <chunkitmax1125@gmail.com>
2020-06-12 00:42:50 +0800

    upload new test

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,7 @@
-// import 'mocha';
+import 'mocha';
 
-// import Assert from 'assert';
-// import Util from 'util';
+import Assert, { AssertionError } from 'assert';
+import Util from 'util';
 
 // /** It is normal to have error on the next line
 //  * because the user credential is not provided
@@ -13,100 +13,175 @@
 //  *    "ip": "localhost"
 //  * }
 //  */
-// import UserConfig from '../../user_config.json';
-// import Futu, { Proto } from '../Futu_';
-// import { Qot_Common, Trd_Common } from '../proto/proto';
+import FutuConfig from '../../user_config.json';
+import Futu, { Proto } from '../futu';
+import { Qot_Common, Trd_Common } from '../proto/proto';
 
-// const ft = new Futu(UserConfig);
+async function AssertShouldResolve<T>(block: (() => Promise<T>) | Promise<T>, message?: string): Promise<T> {
+  try {
+    if (typeof block === 'function') {
+      return await block()
+    } else if (block instanceof Promise) {
+      return await block
+    } else {
+      throw new Error('Unknown parameter')
+    }
+  } catch (err) {
+    throw new AssertionError({
+      actual: err.toString(),
+      expected: 'No rejection',
+      operator: 'vs',
+      message
+    })
+  }
+}
 
-// describe('Futu', () => {
-//   before(async () => {
-//     await ft.waitForInit()
-//     Assert.notEqual(ft.userID, undefined)
-//     Assert.notEqual(ft.connID, undefined)
-//     Assert.notEqual(ft.header, undefined)
-//   })
-//   after(async () => {
-//     await ft.close()
-//     console.log('Test ends')
-//   })
+const ft = new Futu(FutuConfig)
 
-//   it('getGlobalState', async () => {
-//     const res = await ft.getGlobalState({
-//       userID: ft.userID
-//     })
-//     Assert.notEqual(Proto.GetGlobalState.S2C.verify(res), null)
-//   })
+describe('Futu', () => {
+  before(async () => {
+    await ft.ready
+  })
 
-//   it('qotGetStaticInfo', async () => {
-//     const res = await ft.qotGetStaticInfo({
-//       market: Qot_Common.QotMarket.QotMarket_HK_Security,
-//       secType: Qot_Common.SecurityType.SecurityType_Warrant
-//     })
-//     Assert.notEqual(Proto.Qot_GetStaticInfo.S2C.verify(res), null)
-//   })
+  it('qotGetStaticInfo', async () => {
+    let res = await AssertShouldResolve(ft.qotGetStaticInfo({
+      market: Qot_Common.QotMarket.QotMarket_HK_Security,
+      secType: Qot_Common.SecurityType.SecurityType_Warrant
+    }), 'qotGetStaticInfo Failed')
+    Assert.ok(res.staticInfoList)
+  })
 
-//   it('qotGetTradeDate', async () => {
-//     const res = await ft.qotGetTradeDate({
-//       market: Qot_Common.QotMarket.QotMarket_HK_Security,
-//       beginTime: '2018-01-01 00:00:00',
-//       endTime: '2018-02-01 00:00:00'
-//     })
-//     Assert(res.tradeDateList)
-//     Assert.equal(res.tradeDateList!.length, 23)
-//   })
+  it('qotGetTradeDate', async () => {
+    let res = await AssertShouldResolve(ft.qotGetTradeDate({
+      market: Qot_Common.QotMarket.QotMarket_HK_Security,
+      beginTime: '2018-01-01 00:00:00',
+      endTime: '2018-02-01 00:00:00'
+    }), 'qotGetTradeDate Failed')
+    Assert.ok(res.tradeDateList)
+    Assert.deepEqual(res.tradeDateList!.length, 23)
+  })
 
-//   it('trdGetMaxTrdQtys', async () => {
-//     const res = await ft.trdGetMaxTrdQtys({
-//       header: ft.header!,
-//       orderType: Trd_Common.OrderType.OrderType_Normal,
-//       code: '00700',
-//       price: 100000
-//     });
-//     Assert.notEqual(Proto.Trd_GetMaxTrdQtys.S2C.verify(res), null)
-//   });
+  it('qotGetSecuritySnapshot', async () => {
+    const targetSecurity = {
+      code: '00700',
+      market: Qot_Common.QotMarket.QotMarket_HK_Security
+    }
+    let res = await AssertShouldResolve(ft.qotGetSecuritySnapshot({
+      securityList: [targetSecurity]
+    }), 'qotGetSecuritySnapshot Failed')
+    Assert.ok(res.snapshotList)
+    Assert.deepEqual(res.snapshotList!.length, 1)
+    Assert.deepEqual(res.snapshotList![0].basic.security.code, targetSecurity.code)
+    Assert.deepEqual(res.snapshotList![0].basic.security.market, targetSecurity.market)
+  })
 
-//   it('qotSub', async () => {
-//     let rtList: boolean[] = [], tickerList: boolean[] = []
-//     await ft.qotSub({
-//         isSubOrUnSub: true,
-//         isFirstPush: true,
-//         subTypeList: [Qot_Common.SubType.SubType_Ticker, Qot_Common.SubType.SubType_RT],
-//         isRegOrUnRegPush: true,
-//         securityList: [{
-//           code: 'YMmain',
-//           market: Qot_Common.QotMarket.QotMarket_US_Security
-//         }]
-//       },
-//       tickerData => tickerList.push(Proto.Qot_UpdateTicker.S2C.verify(tickerData) === null),
-//       rtData => rtList.push(Proto.Qot_UpdateRT.S2C.verify(rtData) === null)
-//     )
-//     // unsubscribe after 1min according to the doc
-//     await new Promise(resolve => setTimeout(resolve, 61000))
-//     await ft.qotSub({
-//       isSubOrUnSub: false,
-//       subTypeList: [Qot_Common.SubType.SubType_Ticker],
-//       isRegOrUnRegPush: false,
-//       securityList: [{
-//         code: 'YMmain',
-//         market: Qot_Common.QotMarket.QotMarket_US_Security
-//       }]
-//     })
-//     let tickerListLen = tickerList.length,
-//         rtListLen = rtList.length
-//     await new Promise(resolve => setTimeout(resolve, 3000))
-//     await ft.qotSub({
-//       isSubOrUnSub: false,
-//       isRegOrUnRegPush: false,
-//       isUnsubAll: true
-//     })
-//     let rtListLen2 = rtList.length
-//     await new Promise(resolve => setTimeout(resolve, 3000))
-//     Assert.equal(rtList.reduce((a: number, rt) => { if (rt) a++; return a; }, 0), 0)
-//     Assert.equal(tickerList.reduce((a: number, ticker) => { if (ticker) a++; return a; }, 0), 0)
-//     Assert.equal(tickerListLen, tickerList.length)
-//     Assert.notEqual(rtListLen, rtListLen2)
-//     Assert.equal(rtListLen2, rtList.length)
-//   }).timeout(70000)
+  it('qotRequestHistoryKL', async () => {
+    const targetSecurity = {
+      code: 'YMmain',
+      market: Qot_Common.QotMarket.QotMarket_US_Security
+    }
+    let res = await AssertShouldResolve(ft.qotRequestHistoryKL({
+      beginTime: '2020-01-01 00:00:00',
+      endTime: '2020-01-10 00:00:00',
+      klType: Qot_Common.KLType.KLType_Day,
+      rehabType: Qot_Common.RehabType.RehabType_Forward,
+      security: targetSecurity,
+    }), 'qotRequestHistoryKL Failed')
+    Assert.ok(res.klList)
+    Assert.deepEqual(res.klList!.length, 7)
+    Assert.ok(res.security)
+    Assert.deepEqual(res.security.code, targetSecurity.code)
+    Assert.deepEqual(res.security.market, targetSecurity.market)
+  })
 
-// })
+  it('trdGetMaxTrdQtys', async () => {
+    let res = await AssertShouldResolve(ft.trdGetMaxTrdQtys({
+      orderType: Trd_Common.OrderType.OrderType_Normal,
+      code: '00700',
+      price: 100000
+    }), 'trdGetMaxTrdQtys Failed')
+    Assert.ok(res.maxTrdQtys)
+  })
+
+  // do this test during trading hours
+  //   otherwise, it won't pass because the order can't be placed
+  it('trdPlaceOrder & trdModifyOrder', async () => {
+    let orderID: number|Long
+    {
+      let res = await AssertShouldResolve(ft.trdPlaceOrder({
+        code: '00700',
+        orderType: Trd_Common.OrderType.OrderType_AbsoluteLimit,
+        qty: 100,
+        trdSide: Trd_Common.TrdSide.TrdSide_Buy,
+        price: 1.0,
+        secMarket: Trd_Common.TrdSecMarket.TrdSecMarket_HK
+      }), 'trdPlaceOrder Failed')
+      Assert.ok(res.orderID)
+      orderID = res.orderID!
+    }
+    {
+      let res = await AssertShouldResolve(ft.trdModifyOrder({
+        modifyOrderOp: Trd_Common.ModifyOrderOp.ModifyOrderOp_Cancel,
+        orderID
+      }), 'trdModifyOrder Failed')
+      Assert.ok(res.orderID)
+    }
+  })
+
+  it('qotSub', async () => {
+    let tickerCount = 0,
+        rtCount = 0
+    const targetSecurity = {
+      code: 'YMmain',
+      market: Qot_Common.QotMarket.QotMarket_US_Security
+    }
+    // listeners
+    ft.on<Proto.Qot_UpdateTicker.IS2C>(Qot_Common.SubType.SubType_Ticker, targetSecurity, data => {
+      Assert.deepEqual(data.security.code, targetSecurity.code)
+      Assert.deepEqual(data.security.market, targetSecurity.market)
+      ++tickerCount
+    })
+    ft.on<Proto.Qot_UpdateRT.IS2C>(Qot_Common.SubType.SubType_RT, targetSecurity, data => {
+      Assert.deepEqual(data.security.code, targetSecurity.code)
+      Assert.deepEqual(data.security.market, targetSecurity.market)
+      ++rtCount
+    })
+    // subscribe
+    await AssertShouldResolve(ft.qotSub({
+      isSubOrUnSub: true,
+      isRegOrUnRegPush: true,
+      subTypeList: [Qot_Common.SubType.SubType_Ticker, Qot_Common.SubType.SubType_RT],
+      regPushRehabTypeList: [Qot_Common.RehabType.RehabType_Forward],
+      securityList: [targetSecurity]
+    }), 'qotSub Failed')
+    // unsubscribe after 1min according to the doc
+    await new Promise(resolve => setTimeout(resolve, 60500))
+    // only unsubscribe for ticker
+    await ft.qotSub({
+      isSubOrUnSub: false,
+      subTypeList: [Qot_Common.SubType.SubType_Ticker],
+      isRegOrUnRegPush: false,
+      securityList: [targetSecurity]
+    })
+    let tickerCountLap = tickerCount,
+        rtCountLap = rtCount
+    await new Promise(resolve => setTimeout(resolve, 3000))
+    await ft.qotSub({
+      isSubOrUnSub: false,
+      isRegOrUnRegPush: false,
+      isUnsubAll: true
+    })
+    let rtCountLap2 = rtCount
+    await new Promise(resolve => setTimeout(resolve, 3000))
+    Assert.ok(tickerCount > 0)
+    Assert.ok(rtCount > 0)
+    Assert.deepEqual(tickerCountLap, tickerCount)
+    Assert.ok(rtCountLap < rtCount)
+    Assert.ok(rtCountLap < rtCountLap2)
+    Assert.deepEqual(rtCountLap2, rtCount)
+  }).timeout(75000)
+
+  after(async () => {
+    ft.close()
+  })
+})
