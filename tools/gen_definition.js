61f62caac4618d02b9dc671c49321377b215eaf1
chunkitmax <chunkitmax1125@gmail.com>
2020-06-12 11:09:56 +0800

    remove unused tool

--- tools/gen_definition.js
+++
@@ -1,9 +0,0 @@
-const ProtoName2Id = require('../src/proto/protoid.json')
-const Proto = require('../src/proto/proto')
-
-const content = Object.entries(ProtoName2Id).map(([key, value]) => `
-  
-`).join('')
-
-console.log(content)
-console.log('Finished!')
